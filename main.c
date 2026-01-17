4c212b70b19f0b96b976cbc79a11bd173e148d1b
Selim Efe <uysalselimefe@gmail.com>
2026-01-17 22:04:43 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,49 +4,53 @@
 
 int main()
 {
-    int i,j,n,adimsayisi,a;
+    int i, j, n, adimsayisi, a;
 
     printf("n i giriniz");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
     int matris[99][99];
-    int row=n/2;
-    int col=n/2;
-    a=1;
-    adimsayisi=1;
+    int row = n / 2;
+    int col = n / 2;
+    a = 1;
+    adimsayisi = 1;
 
-    matris[row][col]=a++;
+    matris[row][col] = a++;
 
-    for(i=0;i<(n*n);i++){
+    for (i = 0; i < (n * n); i++)
+    {
 
-        for(j=0;j<adimsayisi && a<=n*n;j++){
+        for (j = 0; j < adimsayisi && a <= n * n; j++)
+        {
             row++;
-            matris[row][col]=a++;
+            matris[row][col] = a++;
         }
-        for(j=0;j<adimsayisi && a<=n*n;j++){
+        for (j = 0; j < adimsayisi && a <= n * n; j++)
+        {
             col--;
-            matris[row][col]=a++;
+            matris[row][col] = a++;
         }
 
         adimsayisi++;
 
-        for(j=0;j<adimsayisi && a<=n*n;j++){
+        for (j = 0; j < adimsayisi && a <= n * n; j++)
+        {
             row--;
-            matris[row][col]=a++;
+            matris[row][col] = a++;
         }
 
-        for(j=0;j<adimsayisi && a<=n*n;j++){
+        for (j = 0; j < adimsayisi && a <= n * n; j++)
+        {
             col++;
-            matris[row][col]=a++;
+            matris[row][col] = a++;
         }
         adimsayisi++;
-
-
-
     }
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            printf("%3d",matris[i][j]);
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%3d", matris[i][j]);
         }
         printf("\n");
     }
